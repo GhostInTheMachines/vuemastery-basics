366d7e3d5a4773836413060055aa3d269161e41d
Dennis Kimble <coder@kimbletechnologies.com>
2019-09-10 12:53:50 -0500

    added: dynamic image changer

--- main.js
+++ main.js
@@ -12,11 +12,13 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg"
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg"
       }
     ],
     sizes: [
@@ -38,6 +40,9 @@ var app = new Vue({
   methods: {
     addToCart() {
       this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
     }
   }
 });
